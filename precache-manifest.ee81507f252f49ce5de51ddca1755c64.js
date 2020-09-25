51b71a99b491856e28246bcfdbc093357631f0bb
Maximilian Coghlan <max.coghlan@icloud.com>
2020-09-25 18:56:07 +0100

    Updates

---
+++ precache-manifest.ee81507f252f49ce5de51ddca1755c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa0b8b458427d4787ecbb7ad25f8630",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "222987ee932cdd020ba3",
+    "url": "/pomodoro-clock/static/css/main.86343650.chunk.css"
+  },
+  {
+    "revision": "2a1c8d12bdf4990a6b64",
+    "url": "/pomodoro-clock/static/js/2.488f0cd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.488f0cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222987ee932cdd020ba3",
+    "url": "/pomodoro-clock/static/js/main.7a13d805.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
